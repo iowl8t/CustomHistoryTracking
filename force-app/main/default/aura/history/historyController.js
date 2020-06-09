c3afc7b8bc25debfde2dee0048792b4930c139e0
owl <hawkoneowl@gmail.com>
2020-06-09 15:48:27 +0300

    v2 - Added select field to display option

--- force-app/main/default/aura/history/historyController.js
+++ force-app/main/default/aura/history/historyController.js
@@ -1,4 +1,26 @@
 ({
+    loadFields : function(component, event, helper) {
+
+        let recordId = component.get("v.recordId");
+        let action = component.get("c.getFields");
+
+        action.setParam("recordId", recordId);
+
+        action.setCallback(this, function(response) {
+            let state = response.getState();
+
+            if(state === 'SUCCESS'){
+                let responseObject = JSON.parse(response.getReturnValue());
+                component.set("v.options", responseObject);
+            } else {
+                console.log('Response state: ' + response.getState());
+            }
+        });
+
+        $A.enqueueAction(action);
+    },
+
+
     getHistory : function(component, event, helper) {
         component.set('v.historycolumns', [
             {label: '№ ', fieldName: 'position', type: 'number', initialWidth: 30},
@@ -9,22 +31,30 @@
             {label: 'Who Changed', fieldName: 'who', type: 'text'}
         ]);
         
-        var recordId = component.get("v.recordId");
+        let fieldName = component.find('select').get("v.value");
+        if (fieldName !== 'chooseOne') {
+            let recordId = component.get("v.recordId");
 
-        var action = component.get("c.getHistoryForRecord");
-        action.setParam("recordId", recordId);
+            let action = component.get("c.getHistoryForRecord");
+            action.setParams({
+                "recordId" : recordId,
+                "fieldName" : fieldName
+            });
 
-        action.setCallback(this, function(response) {
-            var state = response.getState();
+            action.setCallback(this, function(response) {
+                let state = response.getState();
 
-            if(state === 'SUCCESS'){
-                var responseObject = JSON.parse(response.getReturnValue());
-                component.set("v.history", responseObject);
-            } else {
-                console.log('Response state: ' + response.getState());
-            }
-        });
+                if(state === 'SUCCESS'){
+                    let responseObject = JSON.parse(response.getReturnValue());
+                    component.set("v.history", responseObject);
+                } else {
+                    component.set("v.history", {});
+                }
+            });
 
-        $A.enqueueAction(action);
+            $A.enqueueAction(action);
+        } else {
+            component.set("v.history", {});
+        }
     }
 })
